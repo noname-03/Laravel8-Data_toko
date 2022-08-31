039a828f07a462eec194217c9ed5a2f5da8f7301
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2022-08-31 22:09:59 +0700

    add role and set route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,25 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home1', [App\Http\Controllers\HomeController::class, 'home'])->name('home1');
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+
+Route::prefix('/admin')->name('admin.')->middleware(['role:admin', 'auth'])->group(function () {
+    // Route::get('/admin', [OperatorController::class, 'index'])->name('operator');
+
+    Route::get('/home', function () {
+        return 'ini admin';
+    })->name('home');
+    //semua route dalam grup ini hanya bisa diakses oleh operator
+});
+
+// Route::middleware(['auth', 'role:user'])->group(function () {
+
+Route::prefix('/user')->name('user.')->middleware(['role:user', 'auth'])->group(function () {
+    // Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+
+    Route::get('/user', function () {
+        return 'ini user';
+    })->name('home');
+    //semua route dalam grup ini hanya bisa diakses siswa
+});
