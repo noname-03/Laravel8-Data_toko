cc56c1057f65fdbf15cdf2cb09244fa3626b695c
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2022-09-07 01:15:18 +0700

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; Haus! Back Office 2022</span>
                     </div>
                 </div>
             </footer>
